7f49c4185bb358585b29438e8c2b1a7de1231d44
Julian Restrepo <julian@MacBook-Pro-de-Julian.local>
2025-06-12 11:24:21 +0200

    Start Session 2 lab and add variables file

---
+++ Session_2_Terraform_Basics/variables.tf
@@ -0,0 +1,44 @@
+variable "prefix" {
+  type       = string
+  decription = "Prefix for identifying resources"
+}
+
+variable "region" {
+  type       = string
+  decription = "AWS region"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "VPC CIDR block"
+}
+
+variable "subnet1_cidr" {
+  type       = string
+  decription = "Subnet 1 CIDR block"
+}
+
+variable "subnet2_cidr" {
+  type       = string
+  decription = "Subnet 2 CIDR block"
+}
+
+variable "subnet3_cidr" {
+  type       = string
+  decription = "Subnet 3 CIDR block"
+}
+
+variable "subnet4_cidr" {
+  type       = string
+  decription = "Subnet 4 CIDR block"
+}
+
+variable "subnet5_cidr" {
+  type       = string
+  decription = "Subnet 5 CIDR block"
+}
+
+variable "subnet6_cidr" {
+  type       = string
+  decription = "Subnet 6 CIDR block"
+}
