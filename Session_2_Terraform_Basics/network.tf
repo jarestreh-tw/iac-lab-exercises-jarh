826759fef31f8f61d3f638ce4f638de2cdb60907
Julian Restrepo <julian@MacBook-Pro-de-Julian.local>
2025-06-12 12:35:19 +0200

    Added route tables and associations. Define outputs

--- Session_2_Terraform_Basics/network.tf
+++ Session_2_Terraform_Basics/network.tf
@@ -82,8 +82,52 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.subnet_private_1.id
+  subnet_id     = aws_subnet.subnet_private_1.id
   tags = {
     Name = format("%s-nat", var.prefix)
   }
 }
+
+resource "aws_route_table" "public_routetable" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = format("%s-public-route-table", var.prefix)
+  }
+}
+
+resource "aws_route_table" "private_routetable" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = format("%s-private-route-table", var.prefix)
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.subnet_public_1.id
+  route_table_id = aws_route_table.public_routetable.id
+}
+
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = aws_subnet.subnet_public_2.id
+  route_table_id = aws_route_table.public_routetable.id
+}
+
+resource "aws_route_table_association" "private_subnet_1" {
+  subnet_id      = aws_subnet.subnet_private_1.id
+  route_table_id = aws_route_table.private_routetable.id
+}
+
+
+resource "aws_route_table_association" "private_subnet_2" {
+  subnet_id      = aws_subnet.subnet_private_2.id
+  route_table_id = aws_route_table.private_routetable.id
+}
+
